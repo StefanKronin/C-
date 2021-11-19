570f008e8ffa3b3e628f5b5c55820297bf62a71f
StefanKronin <82039631+StefanKronin@users.noreply.github.com>
2021-11-19 13:19:06 +0300

    Update Program.cs

--- L4/L4/Program.cs
+++ L4/L4/Program.cs
@@ -1,8 +1,9 @@
-﻿/*Вариант 6. Составить регулярное выражение, 
+/*Вариант 6. Составить регулярное выражение, 
  определяющее, является ли данная строчка валидным URL адресом 
 (http://www.zcontest.ru, http://zcontest.ru/dir%201/dir_2/program.ext?var1=x&var2=my%20valuezcon.com/index.html#bookmark ).*/
 
 using System;
+using System.Text.RegularExpressions;
 
 namespace L4
 {
@@ -10,7 +11,21 @@ namespace L4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string pattern = @"(https?):\/\/(([0-9a-zа-я-]*[.\/][a-zа-я]*))*(\/(.)*)?";
+            string user_string;
+            Console.WriteLine("Ведите URL для проверки");
+            user_string = Console.ReadLine();
+            if (!Regex.IsMatch(user_string, pattern))
+            {
+
+                while (!Regex.IsMatch(user_string, pattern)){
+                    Console.WriteLine("URL не является валидним.\nВведите снова:");
+                    user_string = Console.ReadLine();
+                }
+            }
+            else {
+                Console.WriteLine("URL является валидним.");
+            }
         }
     }
 }
